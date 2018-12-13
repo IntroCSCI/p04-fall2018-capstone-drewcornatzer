809ab5f2232ae118433632817ec89b202156d2c2
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-12-13 11:15:10 -0800

    Finds ders

--- derInt.cpp
+++ derInt.cpp
@@ -11,19 +11,84 @@ void polynomial::setPolynomial(int numberOfTerms, std::vector <double> powers, s
     coefficients.push_back(numbers[i]);
    }
 }
+void polynomial::setConstant(double constant)
+{
+    constantTerm=constant; 
+}
 
 void polynomial::showPolynomial()
 {
     for(int i=0;i<terms;i++)
     {
-     int lastTerm=terms-1;
-     if(i<lastTerm)
-     {
-        std::cout<<coefficients[i]<<"x^{"<<exponents[i]<<"} + ";
-     }
-     else
-     {
-        std::cout<<coefficients[i]<<"x^{"<<exponents[i]<<"}"<<std::endl;
-     }
+        int lastTerm=terms-1;
+        if(i<lastTerm)
+        {
+            std::cout<<coefficients[i]<<"x^{"<<exponents[i]<<"} + ";
+        }
+        else
+        {
+            std::cout<<coefficients[i]<<"x^{"<<exponents[i]<<"}";
+            
+            if(constantTerm!=0)
+            {
+                std::cout<<" + "<<constantTerm<<std::endl;
+            }
+            else
+            {
+                std::cout<<std::endl;    
+            }
+        }
     }
+}
+
+void polynomial::findDerivative()
+{
+        std::vector <double> derExponents;
+        std::vector <double> derCoefficients;
+        double derExp;
+        double derCo;
+
+        for(int i=0;i<terms;i++)
+        {
+            derExp=exponents[i]-1;
+            derCo=exponents[i]*coefficients[i];
+            derCoefficients.push_back(derCo);
+            derExponents.push_back(derExp);
+        }
+        
+        for(int i=0;i<terms;i++)
+        {
+            int lastTerm=terms-1;
+            if(i<lastTerm)
+            {
+                if(derExponents[i]==0)
+                {
+                    std::cout<<derCoefficients[i]<<" + ";
+                }
+                else if(derExponents[i]==1)
+                {
+                    std::cout<<derCoefficients[i]<<"x + ";
+                }
+                else
+                {
+                    std::cout<<derCoefficients[i]<<"x^{"<<derExponents[i]<<"} + ";
+                }
+            }
+            else
+            {
+                if(derExponents[i]==0)
+                {
+                    std::cout<<derCoefficients[i]<<std::endl;
+                }
+                else if(derExponents[i]==1)
+                {
+                    std::cout<<derCoefficients[i]<<"x"<<std::endl;
+                }
+                else
+                {
+                    std::cout<<derCoefficients[i]<<"x^{"<<derExponents[i]<<"}";
+                    std::cout<<std::endl;
+                }
+            }
+        }
 }
