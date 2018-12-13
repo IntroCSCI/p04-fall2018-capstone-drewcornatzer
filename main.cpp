c246a9a2f8bb6b700383c9bc40b2a280a81564a4
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-12-13 12:41:43 -0800

    Finds derivatives of polynomials

--- main.cpp
+++ main.cpp
@@ -11,35 +11,53 @@ int main()
     vector <double> termCoefficients;
     polynomial function;
     char response;
+    char response2;
 
-    cout<<"How many terms does the polynomial have?"<<endl;
-    cin>>polynomialSize;
-    cout<<"Is there a constant term? (Y)es/(N)o"<<endl;
-    cin>>response;
-    if(response=='Y'||response=='y')
+    do
     {
-        double constant;
-        cout<<"What is the constant?"<<endl;
-        cin>>constant;
-        function.setConstant(constant);
-        polynomialSize--;
-    }
-    for(int i=0;i<polynomialSize;i++)
-    {
-        double exponent;
-        double coefficient;
-        cout<<"Term "<<i+1<<": What is the coefficient?"<<endl;
-        cin>>coefficient;
-        cout<<"Term "<<i+1<<": What is the exponent?"<<endl;
-        cin>>exponent;
-        termExponents.push_back(exponent);
-        termCoefficients.push_back(coefficient);
-    }
+        cout<<"How many terms does the polynomial have?"<<endl;
+        cin>>polynomialSize;
+        cout<<"Is there a constant term? (Y)es/(N)o"<<endl;
+        cin>>response;
+        if(response=='Y'||response=='y')
+        {
+            double constant;
+            cout<<"What is the constant?"<<endl;
+            cin>>constant;
+            function.setConstant(constant);
+            polynomialSize--;
+        }
+        for(int i=0;i<polynomialSize;i++)
+        {
+            double exponent;
+            double coefficient;
+            cout<<"Term "<<i+1<<": What is the coefficient?"<<endl;
+            cin>>coefficient;
+            cout<<"Term "<<i+1<<": What is the exponent?"<<endl;
+            cin>>exponent;
+            termExponents.push_back(exponent);
+            termCoefficients.push_back(coefficient);
+        }
     
-    function.setPolynomial(polynomialSize, termExponents, termCoefficients);
-    cout<<"Here is the polynomial you entered:"<<endl;
-    function.showPolynomial();
-    cout<<endl;
+        function.setPolynomial(polynomialSize, termExponents, termCoefficients);
+        cout<<"Here is the polynomial you entered:"<<endl;
+        function.showPolynomial();
+        cout<<"Is this correct? (Y)es/(N)o"<<endl;
+        cin>>response2;
+        while(response2!='n'&&response2!='N'&&response2!='y'&&response2!='Y')
+        {
+            cout<<"ERROR! Type 'y' for yes or 'n' for no."<<endl;
+            function.showPolynomial();
+            cout<<"Is the above polynomial correct?"<<endl;
+            cin>>response2;
+        }
+        if(response2=='n'||response2=='N')
+        {
+            termExponents.clear();
+            termCoefficients.clear();
+            function.clearPolynomial();
+        }
+    }while(response2!='y'&&response2!='Y');
     cout<<"Here is it's derivative:"<<endl;
     function.findDerivative();
 
