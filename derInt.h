1c86995ea086d60e14a064924d5462472d3874cf
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-12-13 11:27:35 -0800

    Better header file

--- derInt.h
+++ derInt.h
@@ -9,10 +9,13 @@ class polynomial
     std::vector <double> exponents;
     std::vector <double> coefficients;
     int terms;
+    double constantTerm=0;
 
     public:
     void setPolynomial(int, std::vector <double>, std::vector <double>);
+    void setConstant(double);
     void showPolynomial();
+    void findDerivative();
 };
 
 
